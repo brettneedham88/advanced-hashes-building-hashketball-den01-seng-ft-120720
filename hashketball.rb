602ae1a4a3f1da851993dc87969b6159eb21a33d
Brett Needham <brettneedham88@gmail.com>
2020-11-27 18:32:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,8 +25,16 @@ game_hash = {
         slam_dunks: 7
       },
       "Brook Lopez" => {
-        
-      }
+        number: 11, 
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15
+      },
+      ""
   ],
   :away => {
     :team_name => "Charlotte Hornets",
