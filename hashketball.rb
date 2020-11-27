38e07083985771a2f507eb6b0a1421a9c6efa918
Brett Needham <brettneedham88@gmail.com>
2020-11-27 19:26:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ game_hash =
   home: {
     team_name: "Brooklyn Nets",
     colors:["Black", "White"],
-    players: [
+    :players => [
       "Alan Anderson" => {
         number: 0,
         shoe: 16,
@@ -60,7 +60,7 @@ game_hash =
   away: {
     team_name: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
-    players: [
+    :players => [
       "Jeff Adrien" => {
         number: 4, 
         shoe: 18,
