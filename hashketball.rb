1864469255b34e42aee438d5e6b3c8a7ae000f00
Brett Needham <brettneedham88@gmail.com>
2020-11-27 18:53:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,7 +101,14 @@ game_hash =
         slam_dunks: 0
       },
       "Kemba Walker" => {
-        
+        number: 33, 
+        shoe: 15,
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0
       }
   }
 }
